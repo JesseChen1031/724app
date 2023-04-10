09ae359a2b1c3510b1815891467a0152cd09ae7b
Huangxing Chen <xingye1031@gmail.com>
2023-04-10 17:50:31 -0400

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const request = require('request');
 const app = express();
 
 dotenv.config({ path: './.env'})
@@ -17,6 +18,32 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
 app.listen(5000, ()=> {
     console.log("server started on port 5000")
+})
+
+app.use(express.urlencoded({extended: 'false'}))
+app.use(express.json())
+
+
+const rqliteAddr = "localhost:4001/db/execute?pretty&timings"
+
+app.post("/auth/register", (req, res) => {    
+    const { username, name, age, password } = req.body
+
+    // db.query() code goes here
+})
+
+app.post("/auth/login", (req, res) => {  
+    const { username, password} = req.body
+
+    // db.query() code goes here
 })
